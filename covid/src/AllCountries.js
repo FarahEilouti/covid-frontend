85791cbfd497d4d12688d10a41398baab90b4953
Farah Riad Eilouti <fara7.eilouti@gmail.com>
2023-05-07 21:12:46 +0300

    works but API is down

--- covid/src/AllCountries.js
+++ covid/src/AllCountries.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function AllCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    async function fetchCountries() {
+      try {
+        const response = await axios.get('https://api.covid19api.com/summary');
+        const countriesData = response.data.Countries;
+        setCountries(countriesData);
+      } catch (error) {
+        console.error('Error fetching countries:', error);
+      }
+    }
+
+    fetchCountries();
+  }, []);
+
+  const handleAddToMyRecords = (country) => {
+    // Implement your logic to add the country to "My Records"
+    console.log('Add to My Records:', country);
+  };
+
   return (
     <div>
-      <h1>Welcome to the AllCountries Page</h1>
-      <p>This is the content of the AllCountries page.</p>
+      <h1>Welcome to the All Countries Page</h1>
+      <div className="country-cards">
+        {countries.map((country) => (
+          <div className="card" key={country.Country}>
+            <h3>{country.Country}</h3>
+            <p>Total Confirmed Cases: {country.TotalConfirmed}</p>
+            <p>Total Deaths: {country.TotalDeaths}</p>
+            <p>Total Recovered: {country.TotalRecovered}</p>
+            <p>Date: {country.Date}</p>
+            <button onClick={() => handleAddToMyRecords(country)}>Add to My Records</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
